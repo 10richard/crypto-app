7e965e7f9862a3d090800bab1d2bb33a8ca60b04
Richard Ten <rten04@icloud.com>
2023-12-19 15:38:34 -0500

    add usd market cap and volume

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,18 +10,30 @@ import eth from "@/public/images/header/eth.svg";
 const Header = () => {
   const [activeCoins, setActiveCoins] = useState<number>(0);
   const [totalMarkets, setTotalMarkets] = useState<number>(0);
-  const [marketCapChange, setMarketCapChange] = useState<number>(0);
-  // const [totalVolume, setTotalVolume] = useState<number>(0);
+  const [marketCap, setMarketCap] = useState<string>("");
+  const [totalVolume, setTotalVolume] = useState<string>("");
   const [btcMarketCapPercent, setBtcMarketCapPercent] = useState<number>(0);
   const [ethMarketCapPercent, setEthMarketCapPercent] = useState<number>(0);
+
+  const formatNum = (num: number): string => {
+    if (num >= 1_000_000_000_000) {
+      return `${(num / 1_000_000_000_000).toFixed(2)}T`;
+    } else if (num >= 1_000_000_000) {
+      return `${(num / 1_000_000_000).toFixed(2)}B`;
+    } else if (num >= 1_000_000) {
+      return `${(num / 1_000_000).toFixed(2)}M`;
+    } else {
+      return num.toLocaleString();
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getHeaderInfo();
       setActiveCoins(data.active_cryptocurrencies);
       setTotalMarkets(data.markets);
-      setMarketCapChange(Math.round(data.market_cap_change_percentage_24h_usd));
-      // setTotalVolume(data.total_volume);
+      setMarketCap(formatNum(data.total_market_cap.usd));
+      setTotalVolume(formatNum(data.total_volume.usd));
       setBtcMarketCapPercent(Math.round(data.market_cap_percentage.btc));
       setEthMarketCapPercent(Math.round(data.market_cap_percentage.eth));
     };
@@ -41,8 +53,8 @@ const Header = () => {
         <p className="text-[#D1D1D1]">Exchange</p>
         <p>{totalMarkets}</p>
       </div>
-      <div>Market Cap: {marketCapChange}</div>
-      {/* <div>Volume:${totalVolume}B</div> */}
+      <div>Market Cap: {marketCap}</div>
+      <div>Volume:${totalVolume}</div>
       <div className="flex items-center gap-2 w-[130px]">
         <img src={btc.src} alt="" className="w-[25px]" />
         <p>{btcMarketCapPercent}%</p>
