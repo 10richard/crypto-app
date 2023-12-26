7d998a94c0c6c7eae067d4b29fdd49b09e7e119a
Richard Ten <rten04@icloud.com>
2023-12-25 22:58:34 -0500

    create and add NavCoinsConverter component (subnav)

--- app/currency-converter/page.tsx
+++ app/currency-converter/page.tsx
@@ -1,4 +1,5 @@
 import Header from "../components/Header";
+import NavCoinsConverter from "../components/NavCoinsConverter";
 import Navbar from "../components/Navbar";
 
 export default function Page() {
@@ -6,6 +7,7 @@ export default function Page() {
     <>
       <Header />
       <Navbar />
+      <NavCoinsConverter />
     </>
   );
 }
