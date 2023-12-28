41c726bc84e2b32ee60fa0e5e473f94d1157c5a1
Richard Ten <rten04@icloud.com>
2023-12-27 23:01:12 -0500

    finish component displaying bitcoin's prices and volume past 30 days

--- app/components/DuoCharts/DuoCharts.tsx
+++ app/components/DuoCharts/DuoCharts.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { getPast30Days } from "@/app/api/getBitcoinInfo";
+import formatNum from "@/app/utils/formatNum";
 import { useState, useEffect } from "react";
-import { Line } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { CategoryScale, LinearScale, LineElement } from "chart.js";
 
@@ -10,7 +11,9 @@ ChartJS.register(CategoryScale, LinearScale, LineElement);
 
 const DuoCharts = () => {
   const [prices, setPrices] = useState([]);
+  const [lastPrice, setLastPrice] = useState<string>("N/A");
   const [volume, setVolume] = useState([]);
+  const [totalVolume, setTotalVolume] = useState<string>("N/A");
 
   const currDate = new Date();
 
@@ -18,7 +21,16 @@ const DuoCharts = () => {
     const fetchData = async () => {
       const data = await getPast30Days();
       setPrices(data.prices);
-      setVolume(data.total_volumes);
+      setLastPrice(formatNum(data.prices[data.prices.length - 1][1]));
+      setVolume(data.total_volumes.map((arr: Array<number>) => arr[1]));
+      setTotalVolume(
+        formatNum(
+          data.total_volumes.reduce(
+            (sum: number, curr: Array<number>) => sum + curr[1],
+            0
+          )
+        )
+      );
     };
     fetchData();
   }, []);
@@ -80,7 +92,6 @@ const DuoCharts = () => {
     labels: Array.from(Array(volume.length).keys()),
     datasets: [
       {
-        borderColor: "#5E74C9",
         data: volume,
         fill: true,
         backgroundColor: (context: {
@@ -94,8 +105,8 @@ const DuoCharts = () => {
               0,
               context.chart.height
             );
-            linearGradient.addColorStop(0, "#4d5c9e");
-            linearGradient.addColorStop(1, "#191926");
+            linearGradient.addColorStop(0, "#A75EE0");
+            linearGradient.addColorStop(1, "#251d3c");
             return linearGradient;
           }
         },
@@ -109,7 +120,7 @@ const DuoCharts = () => {
         <div className="flex flex-col gap-6 w-[632px] bg-[#191934] rounded-xl p-6">
           <div className="flex flex-col gap-4">
             <p className="text-xl text-[#D1D1D1]">Bitcoin (BTC)</p>
-            <p className="text-[28px] font-bold">$13.431 mln</p>
+            <p className="text-[28px] font-bold">${lastPrice}</p>
             <p className="text-[#B9B9BA]">
               {currDate.toLocaleDateString("en", {
                 month: "long",
@@ -122,8 +133,21 @@ const DuoCharts = () => {
             <Line data={pricesData} options={pricesOpts} />
           </div>
         </div>
-        <div className="w-[632px] bg-[#1F1934] rounded-xl p-6">
-          <Line data={volumeData} options={pricesOpts} />
+        <div className="flex flex-col gap-6 w-[632px] bg-[#191934] rounded-xl p-6">
+          <div className="flex flex-col gap-4">
+            <p className="text-xl text-[#D1D1D1]">Volume 30d</p>
+            <p className="text-[28px] font-bold">{totalVolume}</p>
+            <p className="text-[#B9B9BA]">
+              {currDate.toLocaleDateString("en", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </p>
+          </div>
+          <div className="h-[216px]">
+            <Bar data={volumeData} options={pricesOpts} />
+          </div>
         </div>
       </div>
     </div>
