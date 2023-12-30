6be00c7f44754763868a2e41bfd7d45b16eee31e
Richard Ten <rten04@icloud.com>
2023-12-29 21:46:24 -0500

    add functions to fetch different time periods

--- app/api/getBitcoinInfo.ts
+++ app/api/getBitcoinInfo.ts
@@ -1,6 +1,46 @@
-export async function getPast30Days() {
+export async function getPast5Years() {
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1825&interval=daily"
+  );
+  const json = await data.json();
+  return json;
+}
+
+export async function getPastYear() {
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365&interval=daily"
+  );
+  const json = await data.json();
+  return json;
+}
+
+export async function getPastMonth() {
   const data = await fetch(
     "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30&interval=daily"
+  );
+  const json = await data.json();
+  return json;
+}
+
+export async function getPast14Days() {
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=14&interval=daily"
+  );
+  const json = await data.json();
+  return json;
+}
+
+export async function getPast7Days() {
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7&interval=daily"
+  );
+  const json = await data.json();
+  return json;
+}
+
+export async function getPast24Hours() {
+  const data = await fetch(
+    "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=1"
   );
   const json = await data.json();
   return json;
