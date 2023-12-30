bedd7beed3902a856cd26e2231f2a85f9fa7e4a5
Richard Ten <rten04@icloud.com>
2023-12-30 18:49:57 -0500

    map through fetched tokens

--- app/components/DuoCharts/TokenList.tsx
+++ app/components/DuoCharts/TokenList.tsx
@@ -1,8 +1,24 @@
 import { getTop50Tokens } from "@/app/api/getTopTokens";
 import { useEffect, useState } from "react";
+import PriceChangeContainer from "../TokenTable/PriceChangeContainer";
+
+interface TokenInfo {
+  id: string;
+  image: string;
+  symbol: string;
+  name: string;
+  current_price: number;
+  price_change_percentage_1h_in_currency: number;
+  price_change_percentage_24h_in_currency: number;
+  price_change_percentage_7d_in_currency: number;
+  total_volume: number;
+  market_cap: number;
+  circulating_supply: number;
+  total_supply: number;
+}
 
 const TokenList = () => {
-  const [tokens, setTokens] = useState([]);
+  const [tokens, setTokens] = useState<TokenInfo[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,20 +31,23 @@ const TokenList = () => {
   return (
     <div className="flex flex-col">
       <h2>Select the currency to view statistics</h2>
-      <div className="flex">
-        {/* Map through tokens */}
-        <img src="" alt="" />
-        <div>
-          <p>Token Name</p>
+      {tokens.map((token, idx) => (
+        <div key={idx} className="flex">
+          <img src={token.image} alt="" />
           <div>
-            <p>Token Price</p>
+            <p>
+              {token.name} {token.symbol}
+            </p>
             <div>
-              <img src="" alt="green or red arrow" />
-              <p>Percent change</p>
+              {/* From what time? - 1h, 24h, 7d */}
+              <p>{token.current_price}</p>
+              <PriceChangeContainer
+                priceChange={token.price_change_percentage_1h_in_currency}
+              />
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
