9b42e9410d9c128e9fa0783f12b71a32b3167ef2
Richard Ten <rten04@icloud.com>
2024-01-02 21:14:27 -0500

    change selectedToken and timePeriod to useState and delete handleClick func

--- app/components/DuoCharts/DuoCharts.tsx
+++ app/components/DuoCharts/DuoCharts.tsx
@@ -9,13 +9,13 @@ import TimePeriodSelector from "./TimePeriodSelector";
 import TokenCarousel from "./TokenCarousel";
 
 const DuoCharts = () => {
-  const selectedToken = useRef("Bitcoin (BTC)");
+  const [selectedToken, setSelectedToken] = useState("Bitcoin (BTC)");
   const tokenID = useRef("bitcoin");
   const [prices, setPrices] = useState<Array<[number, number]>>([]);
   const [lastPrice, setLastPrice] = useState<string>("N/A");
   const [volumes, setVolumes] = useState<number[]>([]);
   const [totalVolume, setTotalVolume] = useState<string>("N/A");
-  const timePeriod = useRef("1D");
+  const [timePeriod, setTimePeriod] = useState("1D");
 
   const daysMap: Record<string, string> = {
     "1D": "1",
@@ -32,11 +32,11 @@ const DuoCharts = () => {
   };
 
   const getDataFrequency = (data: Array<[number, number]>) => {
-    if (timePeriod.current === "1D") {
+    if (timePeriod === "1D") {
       return data.filter((data, idx) => {
         return idx % 12 === 0;
       });
-    } else if (timePeriod.current === "1Y" || timePeriod.current === "5Y") {
+    } else if (timePeriod === "1Y" || timePeriod === "5Y") {
       return data.filter((data, idx) => {
         return idx % 30 === 0;
       });
@@ -46,53 +46,46 @@ const DuoCharts = () => {
   };
 
   const getQueryString = () => {
-    config.days = daysMap[timePeriod.current];
+    config.days = daysMap[timePeriod];
     let query = Object.entries(config).reduce(
       (acc, [key, val]) => `${acc}&${key}=${val}`,
       ""
     );
 
-    query += timePeriod.current === "1D" ? "" : "&interval=daily";
+    query += timePeriod === "1D" ? "" : "&interval=daily";
     return query;
   };
 
   const changeToken = (id: string, name: string, symbol: string) => {
-    selectedToken.current = `${name} (${symbol.toUpperCase()})`;
+    setSelectedToken(`${name} (${symbol.toUpperCase()})`);
     tokenID.current = id;
-    fetchData();
-  };
-
-  const handleClick = (period: string) => {
-    timePeriod.current = period;
-    fetchData();
-  };
-
-  const fetchData = async () => {
-    const queryString = getQueryString();
-    const data = await getPastData(tokenID.current, queryString);
-    const prices = getDataFrequency(data.prices);
-    const volumes = getDataFrequency(data.total_volumes);
-    setPrices(prices);
-    setLastPrice(formatNum(data.prices[data.prices.length - 1][1]));
-    setVolumes(volumes.map((arr) => arr[1]));
-    setTotalVolume(
-      formatNum(
-        timePeriod.current === "1D"
-          ? volumes.reduce(
-              (sum: number, curr: Array<number>) => sum + curr[1],
-              0
-            ) / 24
-          : data.total_volumes.reduce(
-              (sum: number, curr: Array<number>) => sum + curr[1],
-              0
-            )
-      )
-    );
   };
 
   useEffect(() => {
+    const fetchData = async () => {
+      const queryString = getQueryString();
+      const data = await getPastData(tokenID.current, queryString);
+      const prices = getDataFrequency(data.prices);
+      const volumes = getDataFrequency(data.total_volumes);
+      setPrices(prices);
+      setLastPrice(formatNum(data.prices[data.prices.length - 1][1]));
+      setVolumes(volumes.map((arr) => arr[1]));
+      setTotalVolume(
+        formatNum(
+          timePeriod === "1D"
+            ? volumes.reduce(
+                (sum: number, curr: Array<number>) => sum + curr[1],
+                0
+              ) / 24
+            : data.total_volumes.reduce(
+                (sum: number, curr: Array<number>) => sum + curr[1],
+                0
+              )
+        )
+      );
+    };
     fetchData();
-  }, []);
+  }, [selectedToken, timePeriod]);
 
   return (
     <div className="flex justify-center mt-[72px]">
@@ -100,19 +93,19 @@ const DuoCharts = () => {
         <TokenCarousel changeToken={changeToken} />
         <div className="flex gap-8">
           <PricesChart
-            token={selectedToken.current}
+            token={selectedToken}
             currPrice={lastPrice}
             prices={prices}
           />
           <VolumeChart
             totalVolume={totalVolume}
             volumes={volumes}
-            timePeriod={timePeriod.current}
+            timePeriod={timePeriod}
           />
         </div>
         <TimePeriodSelector
-          currTimePeriod={timePeriod.current}
-          handleClick={handleClick}
+          currTimePeriod={timePeriod}
+          handleClick={setTimePeriod}
         />
       </div>
     </div>
