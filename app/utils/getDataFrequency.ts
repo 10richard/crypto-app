11ecc77885c8e33bd6c1ec9df19159975912580c
Richard Ten <rten04@icloud.com>
2024-01-08 23:14:13 -0500

    create getDataFrequency file

---
+++ app/utils/getDataFrequency.ts
@@ -0,0 +1,16 @@
+export default function getDataFrequency(
+  data: Array<[number, number]>,
+  timePeriod: string
+) {
+  if (timePeriod === "1D") {
+    return data.filter((data, idx) => {
+      return idx % 12 === 0;
+    });
+  } else if (timePeriod === "1Y" || timePeriod === "5Y") {
+    return data.filter((data, idx) => {
+      return idx % 30 === 0;
+    });
+  } else {
+    return data;
+  }
+}
