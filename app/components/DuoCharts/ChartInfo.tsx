752a02b8508104dfd27255f4e52eda554caea3f0
Richard Ten <rten04@icloud.com>
2024-01-11 08:52:21 -0500

    add includeDate prop

--- app/components/DuoCharts/ChartInfo.tsx
+++ app/components/DuoCharts/ChartInfo.tsx
@@ -1,16 +1,17 @@
 interface ChartInfoProps {
   title: string;
   value: string;
+  includeDate: boolean;
 }
 
-const ChartInfo = ({ title, value }: ChartInfoProps) => {
+const ChartInfo = ({ title, value, includeDate }: ChartInfoProps) => {
   const currDate = new Date();
 
   return (
     <div className="flex flex-col gap-4">
       <p className="text-xl text-[#D1D1D1]">{title}</p>
       <p className="text-[28px] font-bold">{value}</p>
-      <p className="text-[#B9B9BA]">
+      <p className={`text-[#B9B9BA] ${includeDate ? "" : "hidden"}`}>
         {currDate.toLocaleDateString("en", {
           month: "long",
           day: "numeric",
