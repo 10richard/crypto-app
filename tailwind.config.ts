30a8bf023afe232716a76ab8e335828e212452a3
Richard Ten <rten04@icloud.com>
2024-01-15 11:27:44 -0500

    delete backgroundImage in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,11 +13,6 @@ const config: Config = {
         bkg: "var(--main-bg)",
         content: "var(--main-text)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
