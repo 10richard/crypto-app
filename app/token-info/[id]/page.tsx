855214ba421b4a961d63fc3c1f5d6b54d02f93eb
Richard Ten <rten04@icloud.com>
2024-01-18 09:59:52 -0500

    Add TokenInfo component to page

--- app/token-info/[id]/page.tsx
+++ app/token-info/[id]/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/app/components/Header";
 import Navbar from "@/app/components/Navbar";
+import TokenInfo from "@/app/components/TokenInfo/TokenInfo";
 
 export default function Page({ params }: { params: { id: string } }) {
   return (
     <>
       <Header />
       <Navbar />
+      <TokenInfo token={params.id} />
     </>
   );
 }
