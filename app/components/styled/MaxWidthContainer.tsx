7fd50f80b9d8f372b65020a56b479c585cf6508b
Richard Ten <rten04@icloud.com>
2024-01-21 17:11:59 -0500

    install styled-components and create MaxWidthContainer

---
+++ app/components/styled/MaxWidthContainer.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import styled from "styled-components";
+
+export const MaxWidthContainer = styled.div`
+  max-width: 1296px;
+  margin: 0 auto;
+  width: 100%;
+  padding: 0 30px;
+`;
