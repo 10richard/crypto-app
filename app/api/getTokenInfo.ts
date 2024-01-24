cfba09d2d92c3d8d3292294802a9a19cbf6ca635
Richard Ten <rten04@icloud.com>
2024-01-24 08:26:47 -0500

    edit getTokenInfo api file

--- app/api/getTokenInfo.ts
+++ app/api/getTokenInfo.ts
@@ -1,5 +1,7 @@
 export async function getTokenInfo(token_id: string) {
-  const data = fetch(
+  const data = await fetch(
     `https://api.coingecko.com/api/v3/coins/${token_id}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
   );
+  const json = (await data).json();
+  return json;
 }
