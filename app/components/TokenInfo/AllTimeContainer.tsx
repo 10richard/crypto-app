c097cc6ab1ed9a9986b98d1a68f86169b212773e
Richard Ten <rten04@icloud.com>
2024-01-28 23:47:04 -0500

    edit styling and format date

--- app/components/TokenInfo/AllTimeContainer.tsx
+++ app/components/TokenInfo/AllTimeContainer.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface AllTimeContainerProps {
   title: string;
   price: number | undefined;
@@ -5,15 +7,32 @@ interface AllTimeContainerProps {
 }
 
 const AllTimeContainer = ({ title, price, date }: AllTimeContainerProps) => {
+  const dateObj = new Date(date ? date : "");
+  const formattedDate = dateObj.toLocaleDateString("en", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
   return (
-    <div className="flex gap-4">
-      <img src="arrow" alt="" />
-      <div>
-        <div className="flex gap-4">
-          <p>All time {title}: </p>
-          <p className="text-xl font-medium">${price}</p>
-        </div>
-        <div>{date}</div>
+    <div className="flex flex-col">
+      <div className="flex items-center gap-4">
+        <Image
+          src={
+            title === "high"
+              ? "/images/token-info/green-arrow.svg"
+              : "/images/token-info/red-arrow.svg"
+          }
+          alt="Price arrow"
+          width={16}
+          height={16}
+        ></Image>
+        <p>All time {title}: </p>
+        <p className="text-xl font-medium">${price}</p>
+      </div>
+      <div className="text-sm text-[#B9B9BA] flex justify-end max-w-[230px]">
+        {formattedDate}
       </div>
     </div>
   );
