85c83f3776bd7b80a611d4b7b383152312575f4b
Richard Ten <rten04@icloud.com>
2024-01-30 08:51:59 -0500

    add ProgressBar component

---
+++ app/components/PrgoressBar.tsx
@@ -0,0 +1,26 @@
+interface ProgressBarProps {
+  left_text: string;
+  right_text: string;
+  percent: number;
+}
+
+const PrgoressBar = ({ left_text, right_text, percent }: ProgressBarProps) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="text-xs flex justify-between">
+        <p>{left_text}</p>
+        <p className="text-[#5E74C9]">{right_text}</p>
+      </div>
+      <div className="h-[6px] w-full bg-[#3C4777] rounded-xl overflow-hidden">
+        <div
+          className={`h-full bg-[#5E74C9]`}
+          style={{
+            width: `${percent}%`,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default PrgoressBar;
