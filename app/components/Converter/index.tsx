61a6695b997df5f756c72ed3fe6248497a86f435
Richard Ten <rten04@icloud.com>
2024-01-30 10:17:39 -0500

    set up html structure

--- app/components/Converter/index.tsx
+++ app/components/Converter/index.tsx
@@ -1,5 +1,51 @@
+import PricesChart from "../DuoCharts/PricesChart";
+import TimePeriodSelector from "../DuoCharts/TimePeriodSelector";
+
 const Converter = () => {
-  return <div>Converter</div>;
+  return (
+    <div className="pb-[70px]">
+      <div>
+        <h2>Online currency convertor</h2>
+        <p>Date Today</p>
+      </div>
+      <div>
+        <div>
+          <div>
+            <h3>You sell</h3>
+            <div>
+              <div>
+                <div>
+                  <img src="token logo" alt="" />
+                  <p>Bitcoin (BTC)</p>
+                  <img src="chevron down" alt="" />
+                </div>
+                <input type="text" />
+              </div>
+              <div className="border border-t-[1px]"></div>
+            </div>
+          </div>
+          <div>
+            <h3>You buy</h3>
+            <div>
+              <div>
+                <div>
+                  <img src="token logo" alt="" />
+                  <p>Ethereum (ETH)</p>
+                  <img src="chevron down" alt="" />
+                </div>
+                <input type="text" />
+              </div>
+              <div className="border border-t-[1px]"></div>
+            </div>
+          </div>
+          <div>
+            {/* <PricesChart /> */}
+            {/* <TimePeriodSelector /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Converter;
