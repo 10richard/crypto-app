413aa0f9b056e88ed2945a8593bed4f2d9a32577
Richard Ten <rten04@icloud.com>
2024-02-01 08:50:43 -0500

    add props and edit styling

--- app/components/Converter/TokenContainer.tsx
+++ app/components/Converter/TokenContainer.tsx
@@ -1,19 +1,44 @@
+import Image from "next/image";
+import { useCurrency } from "@/app/contexts/currencyContext";
+
 interface TokenContainerProps {
   title: string;
   tokenImg: string;
   token: string;
+  price: number;
+  currentTheme: string;
+  bgColor: string;
 }
 
-const TokenContainer = () => {
+const TokenContainer = ({
+  title,
+  tokenImg,
+  token,
+  price,
+  currentTheme,
+  bgColor,
+}: TokenContainerProps) => {
+  const { currentCurrency } = useCurrency();
+
   return (
-    <div className="flex flex-col gap-10 p-6 w-1/2 bg-chart-price rounded-2xl">
-      <h3 className="text-sm text-content-main/80">You sell</h3>
+    <div className={`flex flex-col gap-10 p-6 w-1/2 rounded-2xl ${bgColor}`}>
+      <h3 className="text-sm text-content-main/80">{title}</h3>
       <div className="flex flex-col gap-6">
         <div className="flex justify-between">
-          <div>
-            <img src="token logo" alt="" />
-            <p className="text-xl font-medium">Bitcoin (BTC)</p>
-            <img src="chevron down" alt="" />
+          <div className="flex items-center gap-1">
+            <Image
+              src={tokenImg}
+              alt={`${token} Image`}
+              width={25}
+              height={25}
+            ></Image>
+            <p className="text-xl font-medium">{token}</p>
+            <Image
+              src={`/images/navbar/${currentTheme}/chevron-down.svg`}
+              alt="Chevron down"
+              width={20}
+              height={20}
+            ></Image>
           </div>
           <input
             type="text"
@@ -23,7 +48,9 @@ const TokenContainer = () => {
         </div>
         <div className="p-2 border-t-[1px]">
           <p>
-            <span className="text-content-main/80">1 BTC =</span> $26,250.15
+            <span className="text-content-main/80">1 BTC =</span>{" "}
+            {currentCurrency.symbol}
+            {price}
           </p>
         </div>
       </div>
