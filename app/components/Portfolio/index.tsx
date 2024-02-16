36f29825423444fc21a59bb6e29ceecff948da9e
Richard Ten <rten04@icloud.com>
2024-02-15 19:30:55 -0500

    Add Portfolio component to page

---
+++ app/components/Portfolio/index.tsx
@@ -0,0 +1,7 @@
+import { MaxWidthContainer } from "../styled/MaxWidthContainer";
+
+const Portfolio = () => {
+  return <MaxWidthContainer>Portfolio</MaxWidthContainer>;
+};
+
+export default Portfolio;
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Portfolio from "../components/Portfolio";
 
 export default function Page() {
   return (
     <>
       <Navbar />
+      <Portfolio />
     </>
   );
 }
