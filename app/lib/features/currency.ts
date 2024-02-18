1cce72e0df10bd0c28bff36f6dd28f5495876fef
Richard Ten <rten04@icloud.com>
2024-02-18 12:59:19 -0500

    add boilerplate and create reducers for colorTheme and currency

---
+++ app/lib/features/currency.ts
@@ -0,0 +1,17 @@
+import { createReducer, createAction } from "@reduxjs/toolkit";
+
+const UPDATE_CURRENCY = "UPDATE_CURRENCY";
+
+const currencyReducer = createReducer(
+  { abbr: "usd", symbol: "$" },
+  (builder) => {
+    builder.addCase(UPDATE_CURRENCY, (state, action: any) => {
+      state = action.payload;
+      return state;
+    });
+  }
+);
+
+const updateCurrency = createAction(UPDATE_CURRENCY);
+
+export default currencyReducer;
