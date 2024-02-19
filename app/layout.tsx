d154eb561c0d9d880b6d4cf21c43356302404dae
Richard Ten <rten04@icloud.com>
2024-02-18 19:35:13 -0500

    add StoreProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./contexts/themeContext";
 import { CurrencyProvider } from "./contexts/currencyContext";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
-          <CurrencyProvider>{children}</CurrencyProvider>
-        </ThemeProvider>
+        <StoreProvider>
+          <ThemeProvider>
+            <CurrencyProvider>{children}</CurrencyProvider>
+          </ThemeProvider>
+        </StoreProvider>
       </body>
     </html>
   );
