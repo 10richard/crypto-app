ae435a813d30933520a551721bd24c65686fa158
Richard Ten <rten04@icloud.com>
2024-02-19 09:04:05 -0500

    add Header component

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,11 @@
+import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Portfolio from "../components/Portfolio";
 
 export default function Page() {
   return (
     <>
+      <Header />
       <Navbar />
       <Portfolio />
     </>
