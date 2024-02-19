b7c7da08ae056109311c97770cbaacc1df3a900c
Richard Ten <rten04@icloud.com>
2024-02-19 09:00:19 -0500

    modify structure to latest design (v3)

--- app/components/Portfolio/AssetContainer.tsx
+++ app/components/Portfolio/AssetContainer.tsx
@@ -1,29 +1,46 @@
-const AssetContainer = () => {
+interface SavedAssetInfo {
+  id: string;
+  name: string;
+  coinAmt: number;
+  amtValue: number;
+  pirceChangeSincePurchase: number;
+  purchaseDate: string;
+}
+
+interface CurrenctAssetInfo {}
+
+interface AssetContainerProps {
+  asset: SavedAssetInfo;
+}
+
+const AssetContainer = ({ asset }: AssetContainerProps) => {
   return (
     <div>
       <div>
-        <img src="" alt="token img" />
-        <p>Token Name</p>
+        <div>
+          <img src="" alt="Token img" />
+          <p>Token Name</p>
+        </div>
+        <div>
+          <h3>Total Value</h3>
+          <div>
+            <h2>Amout value (coin amt * current price)</h2>
+            <div>
+              <img
+                src=""
+                alt="red or green arrow (Amount price change since purchase - neg or pos)"
+              />
+              <p>Amount price change since purchase</p>
+            </div>
+          </div>
+          <p>Purchased Date</p>
+        </div>
       </div>
       <div>
-        <div>
-          <h2>Market Price</h2>
-          <div>
-            <div>Current Price</div>
-            <div>Price change 24h</div>
-            <div>Market Cap vs Volume</div>
-            <div>Circ supply by max supply</div>
-          </div>
-        </div>
-        <div className="border border-t-white">
-          <h2>Your coin</h2>
-          <div className="flex">
-            <div>Coint amount</div>
-            <div>Amount value</div>
-            <div>Amount price change since purchase</div>
-            <div>Purchase date</div>
-          </div>
-        </div>
+        <div>Current Price</div>
+        <div>Price change 24h</div>
+        <div>Market cap vs volume</div>
+        <div>Circ supply vs max supply</div>
       </div>
     </div>
   );
