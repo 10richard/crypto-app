a73c64f339af7ef06cc8ddaa4235353436056f8d
Richard Ten <rten04@icloud.com>
2024-02-18 19:36:01 -0500

    add interface and "initial_state" variable

--- app/lib/features/currency.ts
+++ app/lib/features/currency.ts
@@ -1,17 +1,20 @@
 import { createReducer, createAction } from "@reduxjs/toolkit";
 
+interface Currency {
+  abbr: string;
+  symbol: string;
+}
+
 const UPDATE_CURRENCY = "UPDATE_CURRENCY";
+const initial_state = { abbr: "usd", symbol: "$" };
 
-const currencyReducer = createReducer(
-  { abbr: "usd", symbol: "$" },
-  (builder) => {
-    builder.addCase(UPDATE_CURRENCY, (state, action: any) => {
-      state = action.payload;
-      return state;
-    });
-  }
-);
+const currencyReducer = createReducer(initial_state, (builder) => {
+  builder.addCase(UPDATE_CURRENCY, (state, action: any) => {
+    state = action.payload;
+    return state;
+  });
+});
 
-export const updateCurrency = createAction(UPDATE_CURRENCY);
+export const updateCurrency = createAction<Currency>(UPDATE_CURRENCY);
 
 export default currencyReducer;
