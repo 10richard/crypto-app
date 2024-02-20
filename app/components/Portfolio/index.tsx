6487017b2fb4883bd0451a48f952632215c3f219
Richard Ten <rten04@icloud.com>
2024-02-20 08:55:12 -0500

    add comments and more structuring

--- app/components/Portfolio/index.tsx
+++ app/components/Portfolio/index.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MaxWidthContainer } from "../styled/MaxWidthContainer";
+import { getTokenInfo } from "@/app/api/getTokenInfo";
 import AddAssetContainer from "./AddAssetContainer";
+import AssetContainer from "./AssetContainer";
+import { getAllTokens } from "@/app/api/getAllTokens";
 
+// Have 2 different interfaces/states?
 interface AssetInfo {
-  assetName: string;
+  id: string;
+  name: string;
+  current_price: number;
   coinAmt: number;
   amtValue: number;
   pirceChangeSincePurchase: number;
@@ -13,18 +19,42 @@ interface AssetInfo {
 }
 
 const Portfolio = () => {
-  // Fetch coin info for all available assets
-
   // Change the state component to redux
   // Allow user to edit asset info (coin amt and purchase date)
   // Price change since purchase --> subtract current price by bought price (difference) then divide difference by current price and multiply by 100
   const [assets, setAssets] = useState<AssetInfo[]>([]);
   const [toggleModal, setToggleModal] = useState(false);
+  const [allTokens, setAllTokens] = useState([]);
+  // Get unique assets (4 entries for bitcoin, no need to fetch 4 times) --> After you get unique entries, fetch current data
+  const [uniqueAssets, setUniqueAssets] = useState([]);
+
+  const fetchTokenInfo = async (asset: AssetInfo) => {
+    const data = await getTokenInfo(asset.id);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getAllTokens();
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    // fetch current data for tokens
+    const fetchData = async () => {
+      const data = await Promise.all(uniqueAssets.map(fetchTokenInfo));
+    };
+
+    fetchData();
+  }, [assets]);
+
+  // Fetch coin info for all saved assets
 
   return (
     <MaxWidthContainer className="pt-11 pb-[70px]">
-      <div className="text-xl flex justify-between items-center w-full">
-        <p>Your statistics</p>
+      <div className="flex justify-between items-center w-full">
+        <p className="text-2xl">Portfolio</p>
         <button
           onClick={() => setToggleModal(true)}
           className="w-[244px] py-3 rounded-md text-white bg-active-btn/50 border border-[#7878FF]"
@@ -32,7 +62,12 @@ const Portfolio = () => {
           Add Asset
         </button>
       </div>
-      <div>{/* Map through available assets */}</div>
+      <div>
+        {/* Map through saved assets */}
+        {assets.map((asset) => (
+          <AssetContainer key={asset.id} asset={asset} />
+        ))}
+      </div>
       <div className={`${toggleModal ? "" : "hidden"}`}>
         <AddAssetContainer handleToggle={setToggleModal} />
       </div>
