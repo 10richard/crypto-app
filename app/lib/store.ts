061c0a58175cafae1540faa60c913f6b7bac8f48
Richard Ten <rten04@icloud.com>
2024-02-20 08:54:14 -0500

    add portfolioReducer to rootReducer

--- app/lib/store.ts
+++ app/lib/store.ts
@@ -2,13 +2,17 @@ import { combineReducers, createStore } from "@reduxjs/toolkit";
 import currencyReducer from "./features/currency";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import portfolioReducer from "./features/portfolio";
 
 export const persistConfig = {
   key: "root",
   storage,
 };
 
-const rootReducer = combineReducers({ currency: currencyReducer });
+const rootReducer = combineReducers({
+  currency: currencyReducer,
+  portfolio: portfolioReducer,
+});
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
